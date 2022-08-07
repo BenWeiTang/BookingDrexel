d0f6ad5881922bad1304cd689a3cf1ed65b699e2
Ben Tang <78770681+BenWeiTang@users.noreply.github.com>
2022-08-07 14:20:00 -0400

    test script setup

--- testdb.py
+++ testdb.py
@@ -1,15 +1,35 @@
-from socket import htonl
 from database import *
 
 if __name__ == "__main__":
+    pass
     usersDB = UserDataBase()
-    usersDB.addUser("Ben", 1234)
-    uid = usersDB.getUserInfo("Ben")[0]
-
     hotelDB = HotelDatabase()
-    hotelDB.addRoom("abc", 0.0, 1)
-    hotelDB.addRoom("def", 0.0, 99)
-    hotelDB.bookRoom("abc", 1, uid)
-    hotelDB.bookRoom("def", 99, uid)
+    
+    # Test: adduser
+    print('Test: add user')
+    print(usersDB.addUser('ben', 1234))
 
-    print(usersDB.getReservedRooms(uid))
+    # Test: get user info
+    print('Test: get user info')
+    print(usersDB.getUserInfo('ben'))
+
+    # Test: add room
+    print('Test: add room. (True)')
+    print(hotelDB.addRoom('abc', 5, 1))
+    hotelDB.addRoom('abc', 5, 2)
+
+    # Test: book room
+    print('Test: book room. (True)')
+    print(hotelDB.bookRoom('abc', 1, 'ben'))
+
+    # Test: is room taken
+    print('Test: is room taken. (True, False)')
+    print(hotelDB.isRoomTaken('abc', 1))
+    print(hotelDB.isRoomTaken('abc', 2))
+
+    # Test: get empty room count
+    print('Test: get empty room count. (1)')
+    print(hotelDB.getEmptyRoomCount('abc'))
+
+    # Test: reserved room
+    print(usersDB.getReservedRooms('ben'))
